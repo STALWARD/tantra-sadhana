53e84dff3ef5093622a12c1bc783429d8bb677b6
STALWARD <94008690+STALWARD@users.noreply.github.com>
2023-12-08 23:41:28 +0530

    Add files via upload

---
+++ constants/index.tsx
@@ -0,0 +1,96 @@
+// NAVIGATION
+export const NAV_LINKS = [  
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/about', key: 'about', label: 'About' },
+  { href: '/services', key: 'services', label: 'Services' },
+  { href: '/blog', key: 'blog ', label: 'Blog ' },
+  { href: '/contact', key: 'contact', label: 'Contact' },
+];
+
+export const aboutMeData = {
+  name: "TANTRA SADHANA",
+  title: "KAULBHASKAR GURU Ji",
+  body: "Guru Ji, popularly known as KAULBHASKAR, is from the lineage of Sri Matsyendra Nath (also known as Machendra Nath) ji. A direct disciple of esteemed KAUL of Prayag, Sri KULBHUSHANANAND NATH, Guru Ji is basically an Urdhvamanayee Upasaka of MAHATRIPURSUNDARI. Sri Kaulbhaskar Ji, has spent more than 30 years painstakingly perfecting his practice of SRI VIDYA UPASANA of highly mysterious DAKSHINAMURTI SAMPRADAYA.",
+  highlightedBody:
+  "Apart from spiritual guidance and education , WE can also help/guide you in performing various tantrik rituals by qualified experts. Just make a schedule with our Astrologers and judge the accuracy of their divine knowledge. Certainly ! our astrologers will out-perform other so called famous astrologers around you.",
+  body2:
+  "We are some of the experts in Tantra & Astrology",
+};
+
+export const guideMeText = {
+  body: "Ignorance is the curse of God and knowledge is the wing wherewith we fly to the HEAVEN. Only with us you will no longer get lost and get lost again, because we already support when there is no support from anywhere. Even when there is complete darkness and darkness all around you, we are still standing to enlighten your path. Invite your friends, relatives and friends to have fun in the wilderness and darkness through the valley of life and reach the top of the mountain of success.",
+};
+  
+export const contactMeText = {
+  title: "We'd love to hear from you! ",
+  body: "Please use the contact form regarding any questions, comments or feedback!",
+};
+  
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+  
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Tantra-Teaching,Initiation & Ordination',
+    icon: '/zeimusu_Sri_Yantra.svg',
+    variant: 'green',
+    description:
+    'We teach Tantra, specially Sri Vidya and Kaul Marg to real knowledge seekers. We help in INITIATION & ORDINATION for deserving devotees.'
+  },
+  {
+    title: 'Tantra Puja & Ritual',
+    icon: '/The_Sri_Yantra_in_diagrammatic_form.svg',
+    variant: 'green',
+    description:
+    " We provide complete solutions for all your's Tantra Puja and Rituals. Make a Schedule for Puja and Rituals with Our Experts."
+  },
+  {
+    title: 'Know your Destiny',
+    icon: '/The_Sri_Yantra_in_diagrammatic_form.svg',
+    variant: 'green',
+    description:
+    'Know the effect of celestial bodies on you and your life. We, from your horoscope or palm or queries put, can say about your destiny. We provide remedial solutions for your better upliftment also.',
+  },
+  {
+    title: 'Gems, Talisman & Yantras',
+    icon: '/The_Sri_Yantra_in_diagrammatic_form.svg',
+    variant: 'green',
+    description:
+    'We can provide consecrated Gems, Talisman and Rosary. For Sadhakas we can also arrange authentic and precise Meru/Kurma puja yantras, specially designed and consecrated by Sri Kaulbhaskar Guru Ji.',
+  },
+];
+  
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Helpful Links',
+  },
+];
+
+export const FOOTER_CONTACT_INFO = [
+  {
+    title: 'Contact Us',
+  },
+];
+  
+export const SOCIALS = {
+  title: 'Find us on Social Media',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/Linkedin.svg',
+    '/telegram.svg',
+    '/whatsapp.svg',
+    '/email.svg',
+    '/phone.svg',
+     
+  ],
+};
