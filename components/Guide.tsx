b4a4139a24c2c2818bd7341938e69dc5cf665263
STALWARD <94008690+STALWARD@users.noreply.github.com>
2023-12-10 10:49:30 +0530

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -18,7 +18,7 @@ const Guide = () => {
 
       <div className="flexCenter max-container relative w-full">
         <Image 
-          src="/boat.png"
+          src="/boat.jpg"
           alt="boat"
           width={1440}
           height={580}
