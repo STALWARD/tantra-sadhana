70ac858d1319962ae877298e691f9413c10bddf5
STALWARD <94008690+STALWARD@users.noreply.github.com>
2023-12-12 20:16:28 +0530

    Update SiteFooter.tsx

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" className="mb-10">
-            <Image src="/assets/logo/logo.svg" alt="logo" width={50} height={50}/>
+            <Image src="/assets/logo/logo.png" alt="logo" width={50} height={50}/>
           </Link>
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
