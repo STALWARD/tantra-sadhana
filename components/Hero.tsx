dcfb436c90a061b2ccf6c38451b53e7683f6064d
STALWARD <94008690+STALWARD@users.noreply.github.com>
2023-12-13 18:42:53 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,9 +4,18 @@ import Link from 'next/link';
 const Hero = () => {
   return (
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 lg:flex-row">
-      <div className="hero-map" />
+      <div className="flexCenter max-container absolute object-cover" >
+        <Image
+          src='/chakra.png'
+          alt='yoga'  
+          width={600}
+          height={600}
+          priority
+          className="h-screen w-screen object-center"
+        />
+			</div>
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2 ">
-        
+       
         <h1 className="bold-40 md:bold-52 lg:bold-88 ">TANTRA SADHANA</h1>
         <p className="regular-16 mt-6 text-black xl:max-w-[520px]">
           We want to be on each of your journeys seeking the satisfaction of meeting with hidden masters of Tantra & Astrology. We can help you on an adventure around the world of Tantra in just a simple way.
@@ -34,7 +43,7 @@ const Hero = () => {
         <div className="flex flex-col w-full gap-3 sm:flex-row ">
           <Link
             href="https://www.kaulbhaskar.com/profile"
-            className=" bg-black px-8 py-4 rounded-md text-white transition-all hover:bg-gold hover:text-black"
+            className=" bg-black px-8 py-4 rounded-xl text-white transition-all hover:bg-gold hover:text-black"
             >
             KNOW <strong>KAULBHASKAR</strong> GURU JI
           </Link>
@@ -42,7 +51,7 @@ const Hero = () => {
       </div>
 
       <div className="relative flex flex-1 items-start">
-        <div className="relative z-10 flex w-[320px] flex-col gap-4 rounded-3xl bg-green-90 px-4 py-4">
+        <div className="relative z-20 flex w-[320px] flex-col gap-4 rounded-3xl bg-green-90 px-4 py-4">
           <div className="flex flex-col">
             <div className="flexBetween">
               <p className="regular-16 text-gray-20">Our Location</p>
