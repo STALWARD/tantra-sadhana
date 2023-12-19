36a88b841338485bc43da7208c95761d0c0c8e9e
STALWARD <94008690+STALWARD@users.noreply.github.com>
2023-12-19 10:32:49 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       <Camp />
       <Guide />
       <Features />
-      <YoutubeVideo />
+      <YouTubeFrame />
       <Mudra />
       <Mentor />
       <Faq />
