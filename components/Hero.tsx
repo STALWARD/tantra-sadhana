387784308912cbd156d813cda2704645769fa2cc
STALWARD <94008690+STALWARD@users.noreply.github.com>
2023-12-22 12:57:21 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 lg:flex-row">
       <div className="flexCenter max-container absolute object-cover" >
         <Image
-          src='/hero-bg.webp'
+          src='/hero-bg.gif'
           alt='sriyantra'  
           width={600}
           height={600}
@@ -16,8 +16,8 @@ const Hero = () => {
       </div>
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2 ml-5 ">
        
-        <h1 className="bold-40 md:bold-52 lg:bold-88">TANTRA SADHANA</h1>
-        <p className="regular-16 mt-6 text-black xl:max-w-[520px]">
+        <h1 className="bold-40 md:bold-52 lg:bold-88 text-white">TANTRA SADHANA</h1>
+        <p className="regular-16 mt-6 text-white xl:max-w-[520px]">
           We want to be on each of your journeys seeking the satisfaction of meeting with hidden masters of Tantra & Astrology. We can help you on an adventure around the world of Tantra in just a simple way.
         </p>
 
@@ -33,16 +33,16 @@ const Hero = () => {
               />
             ))}
           </div>
-            <p className="bold-16 lg:bold-20 text-black">
+            <p className="bold-16 lg:bold-20 text-white">
             100+
-            <span className="regular-16 lg:regular-20 ml-1 text-black">Excellent Reviews</span>
+            <span className="regular-16 lg:regular-20 ml-1 white">Excellent Reviews</span>
             </p>
         </div>
 
         <div className="flex flex-col w-full gap-3 sm:flex-row ">
           <Link
             href="https://www.kaulbhaskar.com/profile"
-            className=" bg-black px-8 py-4 rounded-xl text-white transition-all hover:bg-gold hover:text-black"
+            className=" bg-white px-8 py-4 rounded-xl text-black transition-all hover:bg-gold hover:text-black"
             >
             KNOW <strong>KAULBHASKAR</strong> GURU JI
           </Link>
@@ -50,20 +50,20 @@ const Hero = () => {
       </div>
 
       <div className="relative flex flex-1 items-start">
-        <div className="relative z-20 flex w-[320px] flex-col gap-4 rounded-3xl bg-green-90 px-4 py-4">
+        <div className="relative z-20 flex w-[320px] flex-col gap-4 rounded-3xl bg-white px-4 py-4">
           <div className="flex flex-col">
             <div className="flexBetween">
-              <p className="regular-16 text-gray-20">Our Location</p>
+              <p className="regular-16 text-black">Our Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
-            <p className="bold-20 text-white">Patna, Bihar (IN)</p>
+            <p className="bold-20 text-red">Patna, Bihar (IN)</p>
           </div>
 
           <div className="flex flex-col">
-            <p className="regular-16 block text-gray-20">Whats App</p>
-            <p className="bold-20 text-white">9934418459</p>
-            <p className="regular-16 block text-gray-20">E-mail:</p>
-            <p className="bold-16 text-white">kaultantra@gmail.com</p>
+            <p className="regular-16 block text-black">Whats App</p>
+            <p className="bold-20 text-red">9934418459</p>
+            <p className="regular-16 block text-black">E-mail:</p>
+            <p className="bold-16 text-red">kaultantra@gmail.com</p>
           </div>
         </div>
       </div>
