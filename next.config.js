ea89c27298210155532f6d4b6e36c83c73d6f1e5
STALWARD <94008690+STALWARD@users.noreply.github.com>
2023-12-22 23:08:54 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,8 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = nextConfig;
+
 const withVideos = require("next-videos");
 
 module.exports = withVideos();
-const nextConfig = {
-    async headers() {
-    return [
-      {
-        source: '/:all*(svg|jpg|png|gif)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=9999999999, must-revalidate',
-          }
-        ],
-      },
-    ]
-  },
-}
-
-module.exports = nextConfig
-
-
