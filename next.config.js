d584eb15d9a8d72a70010bbc3fa600f2b4b180b9
STALWARD <94008690+STALWARD@users.noreply.github.com>
2024-01-26 18:36:00 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-    domains: ["source.unsplash.com"],
-    },
+const nextConfig = {},
     async headers() {
     return [
       {
