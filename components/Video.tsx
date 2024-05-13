3ce3ed3a1832e53860181b082420562a54ae6197
STALWARD <94008690+STALWARD@users.noreply.github.com>
2024-05-13 21:25:04 +0530

    Update Video.tsx

--- components/Video.tsx
+++ components/Video.tsx
@@ -117,33 +117,7 @@ export default function YouTubeFrame() {
 					src={"/images/PURNAGIRI.webp"}
 				/>
 				</div>
-				</div>
-					<div className='flex lg:flex-row flex-col items-center justify-center mx-5 gap-5 mt-5'>
-				<Image
-					priority={false}
-					width={400}
-					height={400}
-					aria-label={"image of KAULBHASKAR"}
-					alt={"KAULBHASKAR"}
-					src={"/images/mahakal.webp"}
-				/>
-				<Image
-					priority={false}
-					width={400}
-					height={400}
-					aria-label={"image of KAULBHASKAR"}
-					alt={"KAULBHASKAR"}
-					src={"/images/mahakal-2.webp"}
-				/>
-				<Image
-					priority={false}
-					width={400}
-					height={400}
-					aria-label={"image of KAULBHASKAR"}
-					alt={"KAULBHASKAR"}
-					src={"/images/bagla.webp"}
-				/>
-				</div>
+
 			</div>
 		</section>
 	); 
