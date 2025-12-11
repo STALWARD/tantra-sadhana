a9cb11118d507e1cb955811bef4136fc33fee184
STALWARD <94008690+STALWARD@users.noreply.github.com>
2025-12-11 23:16:25 +0530

    Add files via upload

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,40 @@
+import fs from "fs";
+import Markdown from "markdown-to-jsx";
+import matter from "gray-matter";
+import getPostMetadata from "@/components/getPostMetadata";
+
+const getPostContent = (slug: string) => {
+  const folder = "posts/";
+  const file = `${folder}${slug}.md`;
+  const content = fs.readFileSync(file, "utf8");
+  const matterResult = matter(content);
+  return matterResult;
+};
+
+export const generateStaticParams = async () => {
+  const posts = getPostMetadata();
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+};
+
+type Params = Promise<{ slug: string }>;
+export default async function BlogPage({ params }: { params: Params }) {
+  const { slug } = await params;
+const post = await getPostContent(slug)
+  return (
+    <div>
+      <div className="my-12 text-center">
+        <h1 className="text-2xl text-black">{post.data.title}</h1>
+        <p className="text-red mt-2">{post.data.author}</p>
+        <p className="text-gold mt-2">{post.data.date}</p>
+      </div>
+
+      <article className="lg:regular-20 m-10">
+        <Markdown>{post.content}</Markdown>
+      </article>
+    </div>
+  );
+};
+
+
