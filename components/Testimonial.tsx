2cf65d0a10c38472f8ced7672d343efb277987d3
STALWARD <94008690+STALWARD@users.noreply.github.com>
2025-12-12 23:57:51 +0530

    Update Testimonial.tsx

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -91,7 +91,7 @@ export default class MultipleItems extends Component {
         };
 
         return (
-            <div className=" pt-40 pb-10 sm:pb-32 lg:py-32 bg-yellow-300" id="testimonial">
+            <div className=" pt-10 pb-10 sm:pb-32 lg:py-32 bg-yellow-300" id="testimonial">
                 <div className='mx-auto max-w-7xl sm:py-4 lg:px-8'>
                 <h2 className="1h-82 text-6xl text-black md:text-55xl text-center md:text-start font-semibold"> What Our Lovely <br />Client Says ?</h2>
                     <Slider {...settings}>
@@ -123,4 +123,5 @@ export default class MultipleItems extends Component {
         );
     }
 }
+
 
