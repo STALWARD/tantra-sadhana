1870397dd614a65330322d2fafbb8e88b46cc1c6
STALWARD <94008690+STALWARD@users.noreply.github.com>
2025-12-13 19:03:57 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- /* confiexperimental: {
-    cacheLife: 60, // Example of new caching API in Next.js 16
-  },g options here */
-  bundlePagesRouterDependencies: true /* config options here */
+ bundlePagesRouterDependencies: true /* config options here */
 };
 
 export default nextConfig;
