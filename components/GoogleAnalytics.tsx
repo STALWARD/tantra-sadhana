99aaa06b4ca5d61f5f7a5494a29097d29f7ad301
STALWARD <94008690+STALWARD@users.noreply.github.com>
2025-12-15 09:17:14 +0530

    Update GoogleAnalytics.tsx

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -2,8 +2,19 @@ import Script from "next/script";
 
 const GoogleAnalytics = ({ ga_id }: { ga_id: string }) => (
   <>
-   <script async src="www.googletagmanager.com"></script>
+    <script async src="www.googletagmanager.com"></script>
+    <Script
+      id="google-analytics"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${ga_id}');
+        `,
+      }}
+    ></Script>
   </>
 );
-
 export default GoogleAnalytics;
