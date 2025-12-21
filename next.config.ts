b2f3e5332f0fe9f1d43cd55ccc0b0ee7e3d5c4e1
STALWARD <94008690+STALWARD@users.noreply.github.com>
2025-12-21 10:32:52 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+ // Enables the "use cache" directive and Cache Components (Standard in v16)
+  cacheComponents: true,
+
  /* confiexperimental: {
     cacheLife: 60, // Example of new caching API in Next.js 16
   },g options here */
