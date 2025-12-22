1a2737a72dc2a3caa4f9ddca0c41ff556de5d16a
STALWARD <94008690+STALWARD@users.noreply.github.com>
2025-12-22 19:01:12 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- /* confiexperimental: {
-    cacheLife: 60, // Example of new caching API in Next.js 16
-  },g options here */
-  bundlePagesRouterDependencies: true
+experimental: {
+    legacyBrowsers: false, // Don't serve legacy JS
+  },
+  swcMinify: true, // Use SWC for faster modern JS minification
 };
 
 export default nextConfig;
