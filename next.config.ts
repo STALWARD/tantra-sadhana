8a297704ef6071fdcf4bcf148ff31f2506653861
STALWARD <94008690+STALWARD@users.noreply.github.com>
2025-12-24 19:24:15 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 const nextConfig = {
   experimental: {
-    legacyBrowsers: false
+    legacyBrowsers: false,
+    modern: true,
+    polyfillsOptimization: true
   },
   swcMinify: true,
    // Optimize for modern browsers (aligns with browserslist)
