310e78f4aa3ed9b8d9113831f57a5d7cc345f08c
STALWARD <94008690+STALWARD@users.noreply.github.com>
2026-01-29 20:04:05 +0530

    Update Camp.tsx

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -3,39 +3,38 @@ import Image from "next/image";
 
 const Camp = () => {
   return (
-    <section className="max-w-2xl relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+    <section className="relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20 max-w-7xl mx-auto">
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <Image
-          src='/img-4.webp'
-          alt='yoga'  
+          src="/img-4.webp"
+          alt="yoga pose outdoors"
           width={1440}
           height={580}
           loading="lazy"
-          className="w-screen h-screen object-cover object-center 2xl:rounded-s-xl"
+          className="h-full w-auto object-cover object-center flex-shrink-0 rounded-xl"
         />
         <Image
-          src='/img-2.webp'
-          alt='yoga'  
+          src="/img-2.webp"
+          alt="yoga meditation"
           width={1440}
           height={580}
           loading="lazy"
-          className="w-screen h-screen object-cover object-center 2xl:rounded-s-xl"
+          className="h-full w-auto object-cover object-center flex-shrink-0 rounded-xl"
         />
       </div>
 
-      <div className="flexEnd mt-2 px-4 lg:-mt-60 lg:mr-6">
-        <div className="bg-yellow-200 p-12 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
-          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-black">
+      <div className="flex justify-end mt-2 px-4 lg:-mt-60 lg:mr-6">
+        <div className="bg-yellow-200 p-12 lg:max-w-[500px] xl:max-w-[734px] xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="text-2xl md:text-3xl 2xl:text-6xl capitalize text-black font-normal">
             <strong>Feeling Lost</strong> And Not Knowing The Way?
           </h2>
-          <p className="regular-14 xl:regular-16 mt-5 text-black text-justify">
+          <p className="text-sm xl:text-base mt-5 text-black text-justify">
             Starting from the anxiety of the common man when visiting a new challenge in life, the possibility of getting lost is very large. That's why we are here for those of you who want to be a winner. We shall enlighten your path so that you will never feel darkness.
           </p>
-
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Camp
+export default Camp;
