e5239bb64b72ec4cb012a54c093df3525e2b85a2
STALWARD <94008690+STALWARD@users.noreply.github.com>
2026-01-29 20:00:58 +0530

    Update Camp.tsx

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Camp = () => {
   return (
-    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+    <section className="max-w-2xl relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <Image
           src='/img-4.webp'
