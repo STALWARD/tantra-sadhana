f1ff3c9cf013726756965194f4204965f2dbdd08
STALWARD <94008690+STALWARD@users.noreply.github.com>
2026-03-02 00:04:26 +0530

    Update styles for Camp component layout

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 const Camp = () => {
   return (
     <section className="relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20 max-w-7xl mx-auto">
-      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+      <div className="hide-scrollbar flex h-85 w-full items-start justify-start gap-8 overflow-x-auto lg:h-100 xl:h-160">
         <Image
           src="/img-4.webp"
           alt="yoga pose outdoors"
           width={1440}
           height={580}
           loading="lazy"
-          className="h-full w-auto object-cover object-center flex-shrink-0 rounded-xl"
+          className="h-full w-auto object-cover object-center shrink-0 rounded-xl"
         />
         <Image
           src="/img-2.webp"
@@ -19,12 +19,12 @@ const Camp = () => {
           width={1440}
           height={580}
           loading="lazy"
-          className="h-full w-auto object-cover object-center flex-shrink-0 rounded-xl"
+          className="h-full w-auto object-cover object-center shrink-0 rounded-xl"
         />
       </div>
 
       <div className="flex justify-end mt-2 px-4 lg:-mt-60 lg:mr-6">
-        <div className="bg-yellow-200 p-12 lg:max-w-[500px] xl:max-w-[734px] xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+        <div className="bg-yellow-200 p-12 lg:max-w-125 xl:max-w-183.5 xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
           <h2 className="text-2xl md:text-3xl 2xl:text-6xl capitalize text-black font-normal">
             <strong>Feeling Lost</strong> And Not Knowing The Way?
           </h2>
