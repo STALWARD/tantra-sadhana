a55e21de2e1c9ab0515f211bfd4ada4cb9603695
STALWARD <94008690+STALWARD@users.noreply.github.com>
2026-06-26 20:22:25 +0530

    Remove header configuration for slug routes
    Removed specific header configuration for slug routes.

--- next.config.ts
+++ next.config.ts
@@ -6,16 +6,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        // This matches your [slug] routes specifically
-        source: "/:slug((?!api|_next/static|_next/image|favicon.ico).*)", 
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable", // 1 year
-          },
-        ],
-      },
-      {
+
         source: "/images/:path*", // your custom images folder
         headers: [
           {
