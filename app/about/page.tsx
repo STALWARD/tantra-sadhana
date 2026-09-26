cebc3a1dba00d367327e7e3d2291f30ebeb191f8
STALWARD <94008690+STALWARD@users.noreply.github.com>
2026-09-26 21:11:53 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -59,6 +59,8 @@ const About = () => {
 					<div className="mt-6 flex justify-start">
                       <a 
                         href="https://www.kaulbhaskar.com/profile"
+						target="_blank"
+                        rel="noopener noreferrer"  
                         className="inline-flex items-center gap-2 bg-black hover:bg-blue-700 text-white px-5 py-2.5 rounded-full text-md font-bold transition-transform active:scale-95 shadow-md"
                         >
                         Know More about Kaulbhaskar Guru Ji
